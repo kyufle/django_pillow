6077bbcfd5b86741b6369967ded03bf7784546ab
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2026-04-07 16:36:37 +0200

    creacion de libros con imagen y previsualización

--- minibiblio/minibiblio/admin.py
+++ minibiblio/minibiblio/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Llibre, Usuari
 
-# Register your models here.
+class LlibreAdmin(admin.ModelAdmin):
+    list_display = ('titol', 'autor', 'data_edicio')
+    readonly_fields = ('vista_previa_imatge',)
+    fields = ('titol', 'autor', 'imatge', 'vista_previa_imatge')
+    
+    def vista_previa_imatge(self, obj):
+        if obj.imatge:
+            return format_html('<img src="{}" width="200" style="object-fit: contain;" />', obj.imatge.url)
+        return "No hi ha imatge"
+    
+    vista_previa_imatge.short_description = 'Previsualització'
+
+admin.site.register(Llibre, LlibreAdmin)
+admin.site.register(Usuari)
